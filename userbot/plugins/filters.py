5b86bc422126a93a27bbfaaf537a53a0bcce0ac0
saihitsai <62397588+saihitsai@users.noreply.github.com>
2020-03-25 15:47:52 +0530

    Update filters.py

--- userbot/plugins/filters.py
+++ userbot/plugins/filters.py
@@ -11,7 +11,7 @@ import re
 from telethon import events, utils
 from telethon.tl import types
 from userbot.plugins.sql_helper.filter_sql import get_filter, add_filter, remove_filter, get_all_filters, remove_all_filters
-from userbot.utils import admin_cmd
+
 
 DELETE_TIMEOUT = 0
 TYPE_TEXT = 0
@@ -65,7 +65,7 @@ async def on_snip(event):
                 last_triggered_filters[event.chat_id].remove(name)
 
 
-@borg.on(admin_cmd("savefilter(.*)"))
+@command(pattern="^.savefilter (.*)")
 async def on_snip_save(event):
     name = event.pattern_match.group(1)
     msg = await event.get_reply_message()
@@ -89,13 +89,13 @@ async def on_snip_save(event):
         await event.edit("Reply to a message with `savefilter keyword` to save the filter")
 
 
-@borg.on(admin_cmd("listfilters"))
+@command(pattern="^.listfilters$")
 async def on_snip_list(event):
     all_snips = get_all_filters(event.chat_id)
     OUT_STR = "Available Filters in the Current Chat:\n"
     if len(all_snips) > 0:
         for a_snip in all_snips:
-            OUT_STR += f"ðŸ‘‰ {a_snip.keyword} \n"
+            OUT_STR += f"👉 {a_snip.keyword} \n"
     else:
         OUT_STR = "No Filters. Start Saving using `.savefilter`"
     if len(OUT_STR) > 4096:
@@ -114,14 +114,14 @@ async def on_snip_list(event):
         await event.edit(OUT_STR)
 
 
-@borg.on(admin_cmd("clearfilter (.*)"))
+@command(pattern="^.clearfilter (.*)")
 async def on_snip_delete(event):
     name = event.pattern_match.group(1)
     remove_filter(event.chat_id, name)
     await event.edit(f"filter {name} deleted successfully")
 
 
-@borg.on(admin_cmd("clearallfilters$"))
+@command(pattern="^.clearallfilters$")
 async def on_all_snip_delete(event):
     remove_all_filters(event.chat_id)
     await event.edit(f"filters **in current chat** deleted successfully")
